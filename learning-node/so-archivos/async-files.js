8969bf36488cdde80abdcf1cf5fe77bb6e36eebf
Edinson <sennin7k@gmail.com>
2020-10-16 12:53:39 -0500

    Async-files examples

---
+++ learning-node/so-archivos/async-files.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const file = process.argv[2];
+
+const content = fs.readFile(file, function (err, content) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const lines = content.toString().split('\n').length;
+
+  console.log(lines);
+});
