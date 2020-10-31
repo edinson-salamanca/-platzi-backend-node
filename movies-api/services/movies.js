89483b1a936a0fe29f3c597e8bfb83c4ab8de3d7
Edinson <sennin7k@gmail.com>
2020-10-31 16:43:40 -0500

    ahora se conecta a una base de datos remota, también se implementó el crub con sus respetivos métodos de mogoDB, se usa archivos .env para las configuraciones

--- movies-api/services/movies.js
+++ movies-api/services/movies.js
@@ -1,31 +1,46 @@
 const { moviesMock } = require('../utils/mocks/movies');
+const MongoLib = require('../lib/mongo');
 
 class MoviesService {
-  async getMovies() {
-    const movies = await Promise.resolve(moviesMock);
+  constructor() {
+    this.collection = 'movies';
+    this.mongoDB = new MongoLib();
+  }
+
+  async getMovies({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+
+    const movies = await this.mongoDB.getAll(this.collection, query);
+
     return movies || [];
   }
 
-  async getMovie() {
-    const movie = Promise.resolve(moviesMock[0]);
+  async getMovie({ movieId }) {
+    const movie = await this.mongoDB.get(this.collection, movieId);
     return movie || {};
   }
 
-  async createMovie() {
-    const createMovieId = await Promise.resolve(moviesMock[0].id);
+  async createMovie({ movie }) {
+    const createMovieId = await this.mongoDB.create(this.collection, movie);
     return createMovieId;
   }
 
-  async update() {
-    const updatedMovieId = Promise.resolve(moviesMock[0].id);
+  async update({ movieId, movie = {} }) {
+    const updatedMovieId = await this.mongoDB.update(
+      this.collection,
+      movieId,
+      movie
+    );
     return updatedMovieId;
   }
+
   async updatePatch() {
     const updatePatchMovieId = Promise.resolve(moviesMock[0].id);
     return updatePatchMovieId;
   }
-  async deleteMovie() {
-    const deletedMovieId = Promise.resolve(moviesMock[0].id);
+
+  async deleteMovie({ movieId }) {
+    const deletedMovieId = await this.mongoDB.delete(this.collection, movieId);
     return deletedMovieId;
   }
 }
