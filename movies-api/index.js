817bb9393488048a29b5a84b80d1748c434d1654
Edinson <sennin7k@gmail.com>
2020-10-31 21:48:26 -0500

    se implemento boom para el manejo de errores

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,14 +4,27 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
-const { errorHandler, logErrors } = require('./utils/middleware/errorHandlers');
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
+
+const notFoundHandle = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
+
+// routes
 moviesApi(app);
 
-app.use(errorHandler);
+// catch 404
+app.use(notFoundHandle);
+
+// errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 /* app.get('/', function (req, res) {
   res.send('hello world');
@@ -22,5 +35,6 @@ app.get('/json', function (req, res) {
 });
  */
 app.listen(config.port, function () {
+  // eslint-disable-next-line no-console
   console.log(`listening http://localhost:${config.port}`);
 });
