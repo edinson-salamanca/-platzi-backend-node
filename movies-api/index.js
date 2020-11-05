333746100c2c96d591c366ac0399293ab4316747
Edinson <sennin7k@gmail.com>
2020-11-04 21:01:19 -0500

    Se agrego el middleware helmet

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config/index');
@@ -11,6 +12,8 @@ const {
 } = require('./utils/middleware/errorHandlers');
 
 const notFoundHandle = require('./utils/middleware/notFoundHandler');
+
+app.use(helmet());
 
 // body parser
 app.use(express.json());
