0ec789b5ed44f1e89e261f32314272d7f717ff6f
Edinson <sennin7k@gmail.com>
2020-11-15 21:01:37 -0500

    movieDurationSchema ahora es de tipo number con min de 1 y max de 300

--- movies-api/utils/schemas/movies.js
+++ movies-api/utils/schemas/movies.js
@@ -5,12 +5,12 @@ const movieTitleSchema = joi.string().max(80);
 const movieYearSchema = joi.number().integer().min(1888).max(2077);
 const movieCoverSchema = joi.string().uri();
 const movieDescriptionSchema = joi.string().max(300);
-const movieDurationSchema = joi.string().min(1).max(300);
+const movieDurationSchema = joi.number().min(1).max(300);
 const movieContentRatingSchema = joi.string().max(5);
 const movieSourceSchema = joi.string().uri();
 const movieTagsSchema = joi.array().items(joi.string().max(50));
 
-const createMovieSchema = joi.object({
+const createMovieSchema = {
   title: movieTitleSchema.required(),
   year: movieYearSchema.required(),
   cover: movieCoverSchema.required(),
@@ -19,9 +19,9 @@ const createMovieSchema = joi.object({
   contentRating: movieContentRatingSchema.required(),
   source: movieSourceSchema.required(),
   tags: movieTagsSchema.required(),
-});
+};
 
-const updateMovieSchema = joi.object({
+const updateMovieSchema = {
   title: movieTitleSchema,
   year: movieYearSchema,
   cover: movieCoverSchema,
@@ -30,7 +30,7 @@ const updateMovieSchema = joi.object({
   contentRating: movieContentRatingSchema,
   source: movieSourceSchema,
   tags: movieTagsSchema,
-});
+};
 
 module.exports = {
   movieIdSchema,
